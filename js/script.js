13d7d98f025326211b3f3edc3e158cbef4f5f3ce
jhon faber navia narvaez <jhonnavia999@hotmail.com>
2022-03-26 14:28:28 -0500

    commit

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-let contents = " " ;
+let contents = " ";
 let lyricstag = document.getElementById("letra");
 const audio = document.getElementById("audio");
 audio.addEventListener("play" , sincronizarCancion);
@@ -22,13 +22,13 @@ function readMultipleFiles(evt) {
     }
 }
 
-document.getElementById("audio-upload").addEventListener("change", cargarAudio);
-document.getElementById("lrc-file").addEventListener("change", readMultipleFiles, false);
+let inputAudio = document.getElementById("audio-upload").addEventListener("change", cargarAudio);
+let inputLrc = document.getElementById("lrc-file").addEventListener("change", readMultipleFiles, false);
 
 let allTextLines = " " ;
 let lyrics = [];
 let llave = [];
-let tim = [] ;
+let tim = [];
 let line = " ";
 
 function processData(allText) {
@@ -40,7 +40,7 @@ function next() {
     for (i = 0; i<allTextLines.length; i++){
         if (allTextLines[i].search(/^(\[)(\d*)(:)(.*)(\])(.*)/i) >=0 ) {
             line = allTextLines[i].match(/^(\[)(\d*)(:)(.*)(\])(.*)/i);
-            tim[i] = parseInt(line[2]) * 60 + parseInt(line[4]);
+            tim[i] = parseInt(line[2]) * 60 + parseFloat(line[4]);
             llave[i] = line[5];
             lyrics[i] = line[6];
         }
@@ -56,15 +56,20 @@ function cargarAudio({target}){
     audio.src = urlObj;
 }
 
-
 function sincronizarCancion(){
+
     
     for (let i = 0; i < tim.length; i++) {
-        
-        if (Math.floor(audio.currentTime) ==tim[i]) {
+        if(roundToTwo(audio.currentTime) == 0 & roundToTwo(tim[3]) != 0 & tim[0]== undefined) lyricstag.innerText = "";
+        else if (roundToTwo(audio.currentTime) == roundToTwo(tim[i])){
             lyricstag.innerText = lyrics[i];
         }
     }
-
+    setTimeout("sincronizarCancion()", 0);
+    if(audio.onpause)return ;
+    
 }
 
+function roundToTwo(num) {
+    return +(Math.round(num + "e+2")  + "e-2");
+}
